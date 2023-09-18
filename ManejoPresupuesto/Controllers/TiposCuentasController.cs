679e499c52f2c5b382ab8e18704d3c942e4bd8e3
IxVerdi1518 <estebanfe400@gmail.com>
2023-09-18 18:29:00 -0500

    Agregar archivos de proyecto.

---
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -0,0 +1,19 @@
+﻿using ManejoPresupuesto.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class TiposCuentasController: Controller
+    {
+        public IActionResult Crear()
+        {
+            return View();
+        }
+        [HttpPost]
+        /*El IAction devuleve cualquier tipo de resultado como json,etc*/
+        public IActionResult Crear(TipoCuenta tipoCuenta)
+        {
+            return View();
+        }
+    }
+}
