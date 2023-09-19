33fc89ee73c6705d90e793d3474ad025d87777b8
IxVerdi1518 <estebanfe400@gmail.com>
2023-09-18 20:12:45 -0500

    Tag Helpers para tener el control de lo que salga

--- ManejoPresupuesto/Models/TipoCuenta.cs
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -5,7 +5,9 @@ namespace ManejoPresupuesto.Models
     public class TipoCuenta
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage ="El campo {0} es requerido")]
+        [StringLength(maximumLength:50,MinimumLength =3, ErrorMessage ="La longitud del campo {0} debe de estar entre {2} y {1}")]
+        [Display(Name ="Nombre del Tipo Cuenta")]
         public string Nombre { get; set; }
         public int UsuarioUd { get; set; }
         public int Orden { get; set; }
