96f9f07b2ebe5ef7ffc14168e23a589e1b1c8239
IxVerdi1518 <estebanfe400@gmail.com>
2023-09-18 20:51:57 -0500

    Validaciones desde le front

--- ManejoPresupuesto/Models/TipoCuenta.cs
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -6,22 +6,22 @@ namespace ManejoPresupuesto.Models
     {
         public int Id { get; set; }
         [Required(ErrorMessage ="El campo {0} es requerido")]
-        [StringLength(maximumLength:50,MinimumLength =3, ErrorMessage ="La longitud del campo {0} debe de estar entre {2} y {1}")]
-        [Display(Name ="Nombre del Tipo Cuenta")]
+        //[StringLength(maximumLength:50,MinimumLength =3, ErrorMessage ="La longitud del campo {0} debe de estar entre {2} y {1}")]
+        //[Display(Name ="Nombre del Tipo Cuenta")]
         public string Nombre { get; set; }
         public int UsuarioUd { get; set; }
         public int Orden { get; set; }
 
         //Pruebas de otros tipos de validaciones
-        [Required(ErrorMessage = "El campo {0} es requerido")]
-        [EmailAddress(ErrorMessage ="El campo debe de ser un campo electronico")]
-        public string Email { get; set; }
-        [Range(minimum:18,maximum:130,ErrorMessage ="El valor debe de estar entre {1} y{2}")]
-        public int Edad { get; set; }
-        [Url(ErrorMessage ="El campo debe de ser una url valida")]
-        public string URL { get; set; }
-        [CreditCard(ErrorMessage ="La tarjeta de credito no es valida")]
-        [Display(Name ="Tarjeta de Credito")]
-        public string TarjetaDeCredito { get; set; }
+        //[Required(ErrorMessage = "El campo {0} es requerido")]
+        //[EmailAddress(ErrorMessage ="El campo debe de ser un campo electronico")]
+        //public string Email { get; set; }
+        //[Range(minimum:18,maximum:130,ErrorMessage ="El valor debe de estar entre {1} y{2}")]
+        //public int Edad { get; set; }
+        //[Url(ErrorMessage ="El campo debe de ser una url valida")]
+        //public string URL { get; set; }
+        //[CreditCard(ErrorMessage ="La tarjeta de credito no es valida")]
+        //[Display(Name ="Tarjeta de Credito")]
+        //public string TarjetaDeCredito { get; set; }
     }
 }
