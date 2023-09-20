8ee409fc6f7ac80a9f16b663b142fcc064dcfd68
IxVerdi1518 <estebanfe400@gmail.com>
2023-09-20 18:15:09 -0500

    Actualizar el Tipo de cuenta con metodos de seguridad

--- ManejoPresupuesto/Controllers/TiposCuentasController.cs
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -17,16 +17,19 @@ namespace ManejoPresupuesto.Controllers
             this.repositorioTiposCuentas = repositorioTiposCuentas;
             this.serviciosUsuarios = serviciosUsuarios;
         }
+
         public async Task<IActionResult> Index()
         {
             var usuarioId = serviciosUsuarios.ObtenerUsuarioId();
             var tiposCuentas = await repositorioTiposCuentas.Obtener(usuarioId);
             return View(tiposCuentas);
         }
+
         public IActionResult Crear()
         {
             return View();
         }
+
         [HttpPost]
         /*El IAction devuleve cualquier tipo de resultado como json,etc*/
         public async Task<IActionResult> Crear(TipoCuenta tipoCuenta)
@@ -52,6 +55,38 @@ namespace ManejoPresupuesto.Controllers
             /*Una vez hecho todo, redirecciona al index*/
             return RedirectToAction("Index");
         }
+
+        /*Metodo para poder verificar que es la persona correcta que quiere editar*/
+        [HttpGet]
+        public async Task<IActionResult> Editar(int id)
+        {
+            var usuarioId = serviciosUsuarios.ObtenerUsuarioId();
+            var tipoCuenta = await repositorioTiposCuentas.ObtenerPorId(id, usuarioId);
+            /*Retorna un verdadero si o si, sino sale error, o sino no existe y no encuentra*/
+            if (tipoCuenta is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+            /*Sino redirecciona a la venta para editar*/
+            return View(tipoCuenta);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Editar(TipoCuenta tipoCuenta)
+        {
+            var usuarioId = serviciosUsuarios.ObtenerUsuarioId();
+            var tipoCuentaExiste = await repositorioTiposCuentas.ObtenerPorId(tipoCuenta.Id, usuarioId);
+            /*Comprueba nuevamente*/
+            if (tipoCuentaExiste is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+            /*Si retorna verdadero si o si, actualiza los campos*/
+            await repositorioTiposCuentas.Actualizar(tipoCuenta);
+            /*Y devuelve a la venta de Index*/
+            return RedirectToAction("Index");
+        }
+
         /*Para poder validar sin necesidad de enviar el formulario se tiene que hacer una peticion al servidor de tipo get*/
         [HttpGet]
         public async Task<IActionResult> VerificarExisteTipoCuenta(string nombre)
--- ManejoPresupuesto/Servicios/RepositorioTiposCuentas.cs
+++ ManejoPresupuesto/Servicios/RepositorioTiposCuentas.cs
@@ -7,19 +7,23 @@ namespace ManejoPresupuesto.Servicios
     /*Creacion de la interfaz*/
     public interface IRepositorioTiposCuentas
     {
+        Task Actualizar(TipoCuenta tipoCuenta);
         Task Crear(TipoCuenta tipoCuenta);
         Task<bool> Existe(string nombre, int usuarioId);
         Task<IEnumerable<TipoCuenta>> Obtener(int usuarioId);
+        Task<TipoCuenta> ObtenerPorId(int id, int usuarioId);
     }
     public class RepositorioTiposCuentas: IRepositorioTiposCuentas
     {
         /*Clase que hereda de la interfaz para ocupar*/
         private readonly string connectionString;
+
         public RepositorioTiposCuentas(IConfiguration configuration)
         {
             /*Clase constructora para poder llamar desde appsetings la base de datos*/
             connectionString = configuration.GetConnectionString("DefaultConnection");
         }
+
         public async Task Crear(TipoCuenta tipoCuenta)
         {
             /*Nueva instancia de base de datos*/
@@ -29,6 +33,7 @@ namespace ManejoPresupuesto.Servicios
             /*Devuelve ese id y lo almacena en el modelo de Id*/
             tipoCuenta.Id = id;
         }
+
         /*Validacion para comprobar que ya existe dicho registro y que no se duplique*/
         /*Hace que sea una tarea asincrona y manda el nombre y usuario id porque necesita comprobar el mensaje por id*/
         /*Es bool porque asi comprueba que solo exista, no se necesita el contenido del llamado*/
@@ -39,6 +44,7 @@ namespace ManejoPresupuesto.Servicios
             var existe = await connection.QueryFirstOrDefaultAsync<int>(@"SELECT 1 FROM TiposCuentas WHERE Nombre=@Nombre AND UsuarioId=@UsuarioId", new {nombre,usuarioId});
             return existe==1;
         }
+
         /*Metodo para enlistar todo lo que venga desde la base de datos segun el usuario*/
         /*El Ienumerable sirve como enlistar*/
         public async Task<IEnumerable<TipoCuenta>> Obtener(int usuarioId)
@@ -46,6 +52,22 @@ namespace ManejoPresupuesto.Servicios
             using var connection = new SqlConnection(connectionString);
             /*El queey siguiente es justamente para obteener el select y que va a tomar todo lo que venga y retorna un TipoCuenta*/
             return await connection.QueryAsync<TipoCuenta>(@"SELECT id, Nombre,Orden FROM TiposCuentas WHERE UsuarioId=@UsuarioId", new {usuarioId});
+        }
+
+        /*Metodo para actulizar todos los campos que tenga el modelo*/
+        public async Task Actualizar(TipoCuenta tipoCuenta)
+        {
+            using var connection = new SqlConnection(connectionString);
+            /*Se ocupa el execetue porque el query no devuelve algo, sino que su accion principal es actualizar*/
+            await connection.ExecuteAsync(@"UPDATE TiposCuentas SET Nombre=@Nombre WHERE Id=@Id", tipoCuenta);
+        }
+
+        /*Metodo para validar que el usuario sea el correcto que va a actualziar*/
+        public async Task<TipoCuenta> ObtenerPorId(int id, int usuarioId)
+        {
+            using var connection = new SqlConnection(connectionString);
+            /*Lo que devuelve es el valor verdadero que si existe un unico resultado, lo permita ocupar, caso contrario existe y no lo deja, tiene que reflejar si o si que es unico*/
+            return await connection.QueryFirstOrDefaultAsync<TipoCuenta>(@"SELECT Id,Nombre,Orden FROM TiposCuentas WHERE Id=@Id AND UsuarioId=@UsuarioId", new {id,usuarioId});
         }
     }
 }
