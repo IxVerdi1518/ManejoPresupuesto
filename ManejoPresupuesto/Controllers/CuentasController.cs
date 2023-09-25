e5838d6cb6ac64ddd280d0225daaba35b93a29f2
IxVerdi1518 <estebanfe400@gmail.com>
2023-09-25 18:49:55 -0500

    Formulario de cuentas

---
+++ ManejoPresupuesto/Controllers/CuentasController.cs
@@ -0,0 +1,33 @@
+﻿using ManejoPresupuesto.Models;
+using ManejoPresupuesto.Servicios;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class CuentasController : Controller
+    {
+        private readonly IRepositorioTiposCuentas repositorioTiposCuentas;
+        private readonly IServiciosUsuarios serviciosUsuarios;
+
+        /*Llama a las interfaces antes hechas para ocupar su contenido*/
+        public CuentasController(IRepositorioTiposCuentas repositorioTiposCuentas,IServiciosUsuarios serviciosUsuarios)
+        {
+            this.repositorioTiposCuentas = repositorioTiposCuentas;
+            this.serviciosUsuarios = serviciosUsuarios;
+        }
+
+        /*Metodo para mandar a la vista de crear*/
+        [HttpGet]
+        public async Task<IActionResult> Crear()
+        {
+            var usuarioId = serviciosUsuarios.ObtenerUsuarioId();
+            var tiposCuentas = await repositorioTiposCuentas.Obtener(usuarioId);
+            /*Hace el llamado de lo que esta en el modelo para ocuparlo*/
+            var modelo = new CuentaCreacionViewModel();
+            /*Hace una iteracion donde obtiene el id y el nombre para hacerlo y ocuparlo con el IEnumerable y mostrar en el select, el id es importante aunque no se muestre*/
+            modelo.TiposCuentas = tiposCuentas.Select(x => new SelectListItem(x.Nombre, x.Id.ToString()));
+            return View(modelo);
+        }
+    }
+}
---
+++ ManejoPresupuesto/Models/CuentaCreacionViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ManejoPresupuesto.Models
+{
+    public class CuentaCreacionViewModel:Cuenta
+    {
+        /*Este metodo hereda de cuenta para ocupar todos sus atributos, pero la caracteritica especial es que este tipo de cuentas lo recoge como IEnumerable*/
+        public IEnumerable<SelectListItem> TiposCuentas {  get; set; }
+
+    }
+}
